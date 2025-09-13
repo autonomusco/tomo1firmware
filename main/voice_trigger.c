5ccc6d35e6ec3df6b698ef2c80b00649bb6b3599
Autonomus Co. <97437872+autonomusco@users.noreply.github.com>
2025-09-13 16:48:15 +1000

    Implement logging in voice_trigger functions

--- main/voice_trigger.c
+++ main/voice_trigger.c
@@ -1,2 +1,12 @@
+#include "voice_trigger.h"
 #include "esp_log.h"
-void voice_trigger_init(void){}
+
+static const char *TAG = "VOICE_TRIGGER";
+
+void voice_trigger_init(void) {
+    ESP_LOGI(TAG, "voice_trigger_init called");
+}
+
+void voice_trigger_listen(void) {
+    ESP_LOGI(TAG, "voice_trigger_listen called");
+}
