a81fe51fd93c1292aec07637128498dd72470036
Autonomus Co. <97437872+autonomusco@users.noreply.github.com>
2025-09-13 16:43:52 +1000

    Refactor I2C bus initialization function
    Replaced i2c_master_init and related functions with i2c_bus_init.

--- main/i2c_bus.h
+++ main/i2c_bus.h
@@ -1,7 +1,6 @@
 #ifndef I2C_BUS_H
 #define I2C_BUS_H
-#include "driver/i2c.h"
-void i2c_master_init(void);
-esp_err_t i2c_read_bytes(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, size_t len);
-esp_err_t i2c_write_byte(uint8_t dev_addr, uint8_t reg_addr, uint8_t data);
-#endif
+
+void i2c_bus_init(void);
+
+#endif // I2C_BUS_H
