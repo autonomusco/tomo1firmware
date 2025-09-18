232b15d151013859a06d70d8481b6538b5c5a8d0
Autonomus Co. <97437872+autonomusco@users.noreply.github.com>
2025-09-19 01:34:10 +1000

    Update button.h for button event handling

--- main/button.h
+++ main/button.h
@@ -1,5 +1,12 @@
 #pragma once
-#include <stdbool.h>
-typedef enum { BUTTON_EVENT_NONE=0, BUTTON_EVENT_PRESS, BUTTON_EVENT_LONG } button_event_t;
+
+#include "driver/gpio.h"   // for gpio_num_t
+
+typedef enum {
+    BUTTON_EVENT_PRESS,
+    BUTTON_EVENT_LONG,
+} button_event_t;
+
 typedef void (*button_cb_t)(button_event_t ev);
+
 void button_init(gpio_num_t gpio, button_cb_t cb);
