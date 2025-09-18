9ab87a1e2217cee549eb14df579b37d290b205c7
Autonomus Co. <97437872+autonomusco@users.noreply.github.com>
2025-09-19 00:09:53 +1000

    Refactor i2c_bus.h to use #pragma once
    Replaced include guards with #pragma once and added new I2C functions.

--- main/i2c_bus.h
+++ main/i2c_bus.h
@@ -1,6 +1,5 @@
-#ifndef I2C_BUS_H
-#define I2C_BUS_H
-
-void i2c_bus_init(void);
-
-#endif // I2C_BUS_H
+#pragma once
+#include "driver/i2c.h"
+esp_err_t i2c_bus_init(gpio_num_t sda, gpio_num_t scl, uint32_t freq_hz);
+esp_err_t i2c_read_reg(uint8_t addr, uint8_t reg, uint8_t *data, size_t len);
+esp_err_t i2c_write_reg(uint8_t addr, uint8_t reg, const uint8_t *data, size_t len);
