89f3f687c3127734671a8744fe49bf51b9219319
Autonomus Co. <97437872+autonomusco@users.noreply.github.com>
2025-09-19 13:45:31 +1000

    Modify emergency_button_init to return bool

--- main/emergency_button.h
+++ main/emergency_button.h
@@ -11,5 +11,6 @@ typedef void (*emergency_cb_t)(emergency_event_t ev);
 
 /**
  * Initialize emergency button on given GPIO pin.
+ * Returns true on success.
  */
-void emergency_button_init(gpio_num_t gpio, emergency_cb_t cb);
+bool emergency_button_init(gpio_num_t gpio, emergency_cb_t cb);
